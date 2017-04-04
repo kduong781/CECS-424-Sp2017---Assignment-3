9fe5c7dc05a6ce093e16e46e92c88a4985293be4
Kevin Duong <kduong5@yahoo.com>
2017-04-04 11:38:18 -0700

    updated part2

--- Part Two/part2.erl
+++ Part Two/part2.erl
@@ -1,23 +1,23 @@
 -module(part2).
--export([open/1, dupl/2, table/1]).
+-export([open/1, dupl/2, table/1, sortie/1]).
 
-open(A) ->
+open(A) -> %open file
   {ok, S} = file:open(A,read),
   L1 = io:get_line(S,''),
   string:tokens(L1, " ").
 
 
-dupl(String, List) ->
+dupl(String, List) ->%increment if value are the same else add new tuple of value 1
   case lists:keyfind(String, 1, List) of
       {String, Result} ->
         List2 = lists:keyreplace(String, 1, List, {String, Result+1});
-      false -> List2 = lists:append(List, {String, 1})
+      false -> List2 = lists:append(List, [{String, 1}])
   end.
 
-table(A) ->
-  HashTable = {},
+table(A) ->%unsort
+  HashTable = [],
   lists:foldl(fun dupl/2, HashTable, A).
 
-sort(A) ->
+sortie(A) ->%sort
   HashTable = table(open(A)),
-  lists:sort(HashTable).
+  lists:sort(fun({String1,Value1}, {String2,Value2}) -> {Value1,String1} >= {Value2,String2} end, HashTable).
