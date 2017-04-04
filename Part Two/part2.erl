86dcd6b733a74ecf4fdc23c8cef374e961ad9a52
Kevin Duong <Kevin Duong>
2017-04-03 23:25:52 -0700

    updated part2

--- Part Two/part2.erl
+++ Part Two/part2.erl
@@ -1,5 +1,5 @@
 -module(part2).
--export([open/1, tuple/2]).
+-export([open/1, dupl/2, table/1]).
 
 open(A) ->
   {ok, S} = file:open(A,read),
@@ -7,8 +7,13 @@ open(A) ->
   string:tokens(L1, " ").
 
 
-tuple(String, List) ->
+dupl(String, List) ->
   case lists:keyfind(String, 1, List) of
-      {String, Result} -> lists:key;
+      {String, Result} ->
+        List2 = list:keyreplace(String, 1, List, {String, Result+1});
       false -> List2 = lists:append(List, {String, 1})
   end.
+
+table(A) ->
+  HashTable = {},
+  lists:foldl(fun dupl/2, HashTable, A).
